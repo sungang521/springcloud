f65c021f5ac0a7ffc30c0742152aed4058209533
sungang <735006309@qq.com>
2019-06-02 22:37:33 +0800

    test

---
+++ src/main/java/com/sungang/test/Test.java
@@ -0,0 +1,12 @@
+package com.sungang.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<String, String>();
+        map.put("1","11");
+        map.put("2","22");
+    }
+}
